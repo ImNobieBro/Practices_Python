7b6eeb4539a7731da5ad5b80431102e672622886
ImNobieBro <imnobiebro@gmail.com>
2026-07-10 22:09:21 -0600

    Second practice!
    I dont have any idea for write here :l

---
+++ Second_Practice.py
@@ -0,0 +1,10 @@
+#mi second practice yea! :D
+
+health = 100
+
+while health > 0:
+    print("The chicken is fighting! CurrentHealth: ", health)
+
+    health = health - 20
+
+print("Oh hell no, the chicken is dead now lil bro :v")
