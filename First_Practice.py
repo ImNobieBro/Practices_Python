74cdedea8f2c730ded8d8a5c6b980c206b562a99
ImNobieBro <imnobiebro@gmail.com>
2026-07-10 21:59:16 -0600

    My first practice! I am so happy now >:3
    I am so exited to continue my exercises in python :P

---
+++ First_Practice.py
@@ -0,0 +1,9 @@
+#mi first practice yea! :D
+print("Hello, this is my first practice... i am so exited to start :v")
+
+health = 100
+
+if health > 0:
+    print("The chicken is alive!")
+else:
+    print("Poor chicken, is dead now... :(")
